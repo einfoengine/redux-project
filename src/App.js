4938044e99743c675f5b662a35936114678e7432
Mohammad Shanewaz Al Maruf <einfoengine@gmail.com>
2021-06-02 16:33:18 +0600

    Feature: React rout //We have a dedicated tutorial for react routing
    - Import BrowserRouter, Router, Switch, Route from react-router-dom
    - Make Route component with path and component props
    - Wrap them in switch
    
    * If you check at this point, you may not see the changes because of the fixeed top. However this is totally a static component now. We soon will make it dymaic and let you see all thee products lists and their details.
    
    Next: Product List

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+
+import Header from './containers/Header';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import ProductListing from './containers/ProductListing'
+import ProductDetail from './containers/ProductDetail'
+
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productID" exact component={ProductDetail} />
+          <Route>404 Not found</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
